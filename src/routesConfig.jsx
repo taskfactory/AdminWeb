02f7f45a139cba6eb4f405d6ded49bd925ff2700
frankhtli <frankhtli@tencent.com>
2023-04-07 22:17:26 +0800

    update

--- src/routesConfig.jsx
+++ src/routesConfig.jsx
@@ -4,23 +4,24 @@ import {
 import { SourcePage } from './pages';
 import ErrorPage from './pages/errorPage';
 
-function buildConfig(path, elem, label, icon, children) {
+function buildConfig(path, elem, label, showInMenu, icon, children) {
     return {
         label: label,
         element: elem,
         path: path,
         icon: icon,
         children: children ? children : [],
+        showInMenu: showInMenu === true ? true : false,
     };
 }
 
 const ConfigItems = [
-    buildConfig("source", <SourcePage />, "来源管理", <FolderFilled />),
-    buildConfig("test", <ErrorPage />, "Test", <FolderFilled />),
-    buildConfig("user", <ErrorPage />, "User", <FolderFilled />, [
-        buildConfig("tom", <ErrorPage />, "Tom"),
-        buildConfig("bill", <ErrorPage />, "Bill"),
-        buildConfig("frank", <ErrorPage />, "Frank"),
+    buildConfig("source", <SourcePage />, "来源管理", true, <FolderFilled />),
+    buildConfig("test", <ErrorPage />, "Test", true, <FolderFilled />),
+    buildConfig("user", <ErrorPage />, "User", true, <FolderFilled />, [
+        buildConfig("tom", <ErrorPage />, "Tom", true),
+        buildConfig("bill", <ErrorPage />, "Bill", true),
+        buildConfig("frank", <ErrorPage />, "Frank", true),
     ])
 ];
 
